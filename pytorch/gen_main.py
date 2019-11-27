08f44ded1ea1fe317101bfac2babacad5d65d315
optimass <massimo.p.caccia@gmail.com>
2019-11-27 15:26:02 -0500

    clean-up for generative modeling

--- pytorch/gen_main.py
+++ pytorch/gen_main.py
@@ -5,13 +5,12 @@ from torchvision.utils import save_image
 import pdb
 import wandb
 
-import mir
 from data   import *
 from utils  import get_logger, get_temp_logger, logging_per_task, onehot, distillation_KL_loss, \
-                   naive_cross_entropy_loss, get_grad_vector, get_future_step_parameters
+                   naive_cross_entropy_loss,
 from copy   import deepcopy
 from pydoc  import locate
-from model  import ResNet18, CVAE, MLP, classifier
+from model  import ResNet18, MLP, classifier
 from VAE    import VAE
 from VAE.loss import calculate_loss
 
@@ -19,8 +18,6 @@ from VAE.loss import calculate_loss
 # -----------------------------------------------------------------------------------------
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--unit_test', action='store_true',
-    help='unit testing mode for fast debugging')
 parser.add_argument('-d', '--dataset', type=str, default = 'split_mnist',
     choices=['split_mnist', 'permuted_mnist', 'split_cifar10', 'split_cifar100', 'miniimagenet'])
 parser.add_argument('--n_tasks', type=int, default=-1,
@@ -36,7 +33,8 @@ parser.add_argument('--cls_hiddens', type=int, default=400,
     help='number hidden dim in the classifier')
 parser.add_argument('-bs', '--batch_size', type=int, default=10)
 parser.add_argument('--lr', type=float, default=0.1)
-parser.add_argument('--use_conv', action='store_true')
+parser.add_argument('--use_conv', action='store_true'
+    help='If True, use a ResNet, if not MLP')
 parser.add_argument('-s', '--samples_per_task', type=int, default=-1,
     help='if negative, full dataset is used')
 parser.add_argument('--n_runs', type=int, default=1,
@@ -51,40 +49,14 @@ parser.add_argument('--result_dir', type=str, default='temp',
 parser.add_argument('--name', type=str, default=None,
     help='name of the run in WandB project')
 
-#------ MIR -----#
-parser.add_argument('-m','--method', type=str, default='rand_gen',
-    choices=['no_rehearsal','rand_gen', 'mir_gen'])
 parser.add_argument('-gm', '--gen_method', type=str, default='rand_gen',
-    choices=['no_rehearsal', 'mir_gen', 'rand_gen'])
-parser.add_argument('--reuse_samples', type=int, default=0)
+    choices=['no_rehearsal', 'gen_replay'])
+parser.add_argument('--reuse_samples', type=int, default=0
+    help='at every iteration, resample or not')
 parser.add_argument('--n_mem', type=int, default=1,
     help='number of retrieved memories')
-parser.add_argument('-mc', '--mem_coeff', type=float, default=1.0, # not like Lucas' and Eug's
+parser.add_argument('-mc', '--mem_coeff', type=float, default=1.0,
     help='replay loss relative weight')
-parser.add_argument('--mir_iters', type=int, default=1,
-    help='number of gradient descent iterations for MIR')
-parser.add_argument('-mip', '--mir_init_prior', type=int, default=1,
-    help='if on, we start the mir search at the prior, not the posterior')
-# coeff
-parser.add_argument('-cxc', '--cls_xent_coeff', type=float, default=1.0,
-    help='coeff for the classification loss')
-parser.add_argument('-cec', '--cls_ent_coeff', type=float, default=1.0,
-    help='regularization to encourage samples with high certainty')
-parser.add_argument('-cdc', '--cls_div_coeff', type=float, default=1.0,
-    help='regularization to encourage diversity in retrieved samples for classifier')
-parser.add_argument('-csc', '--cls_shell_coeff', type=float, default=1.,
-    help='regularization such that z stays near the gaussian shell')
-parser.add_argument('-grc', '--gen_rec_coeff', type=float, default=1.0,
-    help='coeff for Reconstruction cost')
-parser.add_argument('-gkc', '--gen_kl_coeff', type=float, default=1.0,
-    help='coeff for KL cost')
-parser.add_argument('-gec', '--gen_ent_coeff', type=float, default=1.0,
-    help='regularization to encourage samples with high certainty')
-parser.add_argument('-gdc', '--gen_div_coeff', type=float, default=1.,
-    help='regularization to encourage diversity in retrieved samples for classifier')
-parser.add_argument('-gsc', '--gen_shell_coeff', type=float, default=1.,
-    help='regularization such that z stays near the gaussian shell')
-#----------------#
 
 #------- Generative Model ------#
 parser.add_argument('-o', '--output_loss', type=str, default=None,
@@ -105,18 +77,6 @@ parser.add_argument('--min_beta', type=float, default=0.0,
     help='min beta for warm-up')
 parser.add_argument('--z_size', type=int, default=64,
     help='how many stochastic hidden units')
-parser.add_argument('-f', '--flow', type=str, default='no_flow',
-    choices=['no_flow'])
-    # choices=['planar', 'iaf', 'householder', 'orthogonal','triangular', 'no_flow'])
-# not implemented yet:
-parser.add_argument('-nf', '--num_flows', type=int, default=4,
-    help='Number of flow layers, ignored in absence of flows')
-parser.add_argument('-nv', '--num_ortho_vecs', type=int, default=8,
-    help="For orthogonal flow: How orthogonal vectors per flow do you need.")
-parser.add_argument('-nh', '--num_householder', type=int, default=8,
-    help="For Householder Sylvester flow: Number of Householder matrices per flow.")
-parser.add_argument('-mhs', '--made_h_size', type=int, default=320,
-    help='Width of mades for iaf. Ignored for all other flows.')
 #---------------------------------#
 
 args = parser.parse_args()
@@ -128,39 +88,19 @@ result_path = os.path.join('Results', args.result_dir)
 if not os.path.exists(result_path): os.mkdir(result_path)
 sample_path = os.path.join(*['Results', args.result_dir, 'samples/'])
 if not os.path.exists(sample_path): os.mkdir(sample_path)
-recon_path = os.path.join(*['Results', args.result_dir, 'reconstructions/'])
-if not os.path.exists(recon_path): os.mkdir(recon_path)
-mir_path = os.path.join(*['Results', args.result_dir, 'mir/'])
-if not os.path.exists(mir_path): os.mkdir(mir_path)
 
 args.cuda = torch.cuda.is_available()
 if args.cuda: args.device = 'cuda:0'
 else: args.device = 'cpu'
 
-
-# TODO(this in utils)
-if args.unit_test:
-    args.n_epochs = 1
-    args.gen_iters = 2
-    args.cls_iters = 2
-    args.batch_size = 5
-    args.max_loss_budget = 2
-    args.max_loss_grad_steps = 2
-
-if args.method == args.gen_method == 'no_rehearsal':
-    args.gen_iters = 0
-    args.fully_train_on_gen = 0
-
-# pre logging stuff
-if args.gen_architecture=='GatedConv':
-    args.gen_depth = 6
-args.cls_mir_gen = 1 if args.method=='mir_gen' else 0
-args.gen_mir_gen = 1 if args.gen_method=='mir_gen' else 0
-
 if args.reproc:
     seed=0
     torch.manual_seed(seed)
     np.random.seed(seed)
+
+# this needs to be fixed:
+if args.gen_architecture=='GatedConv':
+    args.gen_depth = 6
 
 # fetch data
 data = locate('data.get_%s' % args.dataset)(args)
@@ -170,7 +110,6 @@ train_loader, val_loader, test_loader  = [CLDataLoader(elem, args, train=t) \
         for elem, t in zip(data, [True, False, False])]
 
 if args.log != 'off':
-    #os.environ['WANDB_MODE'] = 'dryrun'
     if args.name is None: wandb.init(project=args.result_dir)
     else: wandb.init(project=args.result_dir, name=args.name)
     wandb.config.update(args)
@@ -189,8 +128,6 @@ LOG = get_logger(['gen_loss', 'cls_loss', 'acc'],
 # Begin Run Loop
 for run in range(args.n_runs):
 
-    mir_tries, mir_success = 0, 0
-
     # REPRODUCTIBILITY
     if args.reproc:
         np.random.seed(run)
@@ -202,7 +139,6 @@ for run in range(args.n_runs):
     if args.use_conv:
         cls = ResNet18(args.n_classes, nf=20, input_size=args.input_size)
     else:
-        #cls = MLP(args)
         cls = classifier(args).to(args.device)
 
     opt = torch.optim.SGD(cls.parameters(), lr=args.lr)
@@ -211,17 +147,11 @@ for run in range(args.n_runs):
                 sum([np.prod(p.size()) for p in cls.parameters()]))
 
     # GENERATIVE MODELING
-    if (args.method is not 'no_rehearsal') or (args.gen_method is not 'no_rehearsal'):
-        gen = VAE.VAE(args).to(args.device)
-        #if args.cuda:
-        #    gen = gen.cuda()
-        opt_gen = torch.optim.Adam(gen.parameters())
-        if run == 0:
-            print("number of generator parameters: ",
-                    sum([np.prod(p.size()) for p in gen.parameters()]))
-    else:
-        gen=None
-
+    gen = VAE.VAE(args).to(args.device)
+    opt_gen = torch.optim.Adam(gen.parameters())
+    if run == 0:
+        print("number of generator parameters: ",
+                sum([np.prod(p.size()) for p in gen.parameters()]))
     # INIT
     prev_gen, prev_model = None, None
 
@@ -251,9 +181,9 @@ for run in range(args.n_runs):
                 if sample_amt > args.samples_per_task > 0: break
                 sample_amt += data.size(0)
 
-                #if args.cuda:
                 data, target = data.to(args.device), target.to(args.device)
 
+                # KL cost annealing
                 args.beta = min([(sample_amt) / max([args.warmup, 1.]), args.max_beta])
 
                 #------ Train Generator ------#
@@ -275,17 +205,6 @@ for run in range(args.n_runs):
                             if args.gen_method == 'rand_gen':
                                 mem_x = prev_gen.generate(args.batch_size*args.n_mem).detach()
 
-                            elif args.gen_method == 'mir_gen':
-
-                                mem_x, mir_worked = \
-                                    mir.retrieve_gen_for_gen(args, data, gen, prev_gen, prev_cls)
-
-                                mir_tries += 1
-                                if mir_worked:
-                                    mir_success += 1
-                                    # keep for logging later
-                                    gen_x, gen_mem_x = data, mem_x
-
                         mem_x_mean, z_mu, z_var, ldj, z0, zk = gen(mem_x)
                         mem_gen_loss, mem_rec, mem_kl, _ = calculate_loss(mem_x_mean, mem_x, z_mu,
                                 z_var, z0, zk, ldj, args, beta=args.beta)
@@ -299,13 +218,12 @@ for run in range(args.n_runs):
                 # End Generator Iteration Loop
                 #------------------------------
 
-                if gen is not None:
-                    if i % args.print_every == 0:
-                        print('current VAE loss = {:.4f} (rec: {:.4f} + beta: {:.2f} * kl: {:.2f})'
-                            .format(gen_loss.item(), rec.item(), args.beta, kl.item()))
-                        if task > 0:
-                            print('memory VAE loss = {:.4f} (rec: {:.4f} + beta: {:.2f} * kl: {:.2f})'
-                                .format(mem_gen_loss.item(), mem_rec.item(), args.beta, mem_kl.item()))
+                if i % args.print_every == 0:
+                    print('current VAE loss = {:.4f} (rec: {:.4f} + beta: {:.2f} * kl: {:.2f})'
+                        .format(gen_loss.item(), rec.item(), args.beta, kl.item()))
+                    if task > 0:
+                        print('memory VAE loss = {:.4f} (rec: {:.4f} + beta: {:.2f} * kl: {:.2f})'
+                            .format(mem_gen_loss.item(), mem_rec.item(), args.beta, mem_kl.item()))
 
 
                 #------ Train Classifier-------#
@@ -318,24 +236,12 @@ for run in range(args.n_runs):
                     cls_loss = F.cross_entropy(logits, target, reduction='mean')
                     tot_cls_loss = 0 + cls_loss
 
-                    if task > 0 and args.method != 'no_rehearsal':
+                    if task > 0:
 
                         if it == 0 or not args.reuse_samples:
 
-                            if args.method=='rand_gen':
-                                mem_x = prev_gen.generate(args.batch_size*args.n_mem).detach()
-                                mem_y = torch.softmax(prev_cls(mem_x), dim=1).detach()
-
-                            elif args.method=='mir_gen':
-
-                                mem_x, mem_y, mir_worked = \
-                                    mir.retrieve_gen_for_cls(args, data, cls, prev_cls, prev_gen)
-
-                                mir_tries += 1
-                                if mir_worked:
-                                    mir_success += 1
-                                    # keep for logging later
-                                    cls_x, cls_mem_x = data, mem_x
+                            mem_x = prev_gen.generate(args.batch_size*args.n_mem).detach()
+                            mem_y = torch.softmax(prev_cls(mem_x), dim=1).detach()
 
                         mem_logits = cls(mem_x)
 
@@ -354,7 +260,7 @@ for run in range(args.n_runs):
                     pred = logits.argmax(dim=1, keepdim=True)
                     acc = pred.eq(target.view_as(pred)).sum().item() / pred.size(0)
                     print('current training accuracy: {:.2f}'.format(acc))
-                    if task > 0 and args.method != 'no_rehearsal':
+                    if task > 0:
                         pred = mem_logits.argmax(dim=1, keepdim=True)
                         mem_y = mem_y.argmax(dim=1, keepdim=True)
                         acc = pred.eq(mem_y.view_as(pred)).sum().item() / pred.size(0)
@@ -383,18 +289,6 @@ for run in range(args.n_runs):
 
                 if args.dataset is not 'permuted_mnist':
 
-                    # save some training reconstructions:
-                    recon_path_ = os.path.join(recon_path,'task{}.png'.format(task))
-                    recons = torch.cat([data.to('cpu'), x_mean.to('cpu')])
-                    save_image(recons, recon_path_, nrow=args.batch_size)
-                    if wandb is not None:
-                        logged_og = wandb.Image(data.to('cpu'),
-                            caption="OG task {}".format(task))
-                        wandb.log({"OG Run {}".format(run): logged_og}, step=task)
-                        logged_recons = wandb.Image(x_mean.to('cpu'),
-                            caption="reconstructions task {}".format(task))
-                        wandb.log({"Reconstructions Run {}".format(run): logged_recons}, step=task)
-
                     # save some pretty images:
                     gen_images = gen.generate(25).to('cpu')
                     sample_path_ = os.path.join(sample_path,'task{}.png'.format(task))
@@ -403,20 +297,6 @@ for run in range(args.n_runs):
                         logged_gen = wandb.Image(gen_images[:16],
                             caption="generations task {}".format(task))
                         wandb.log({"Generations Run {}".format(run): logged_gen}, step=task)
-
-                    # save some MIR samples:
-                    if task>0:
-                        if 'mir_gen'==args.method:
-                            mir_images = torch.cat([cls_x.to('cpu'),
-                                    cls_mem_x.to('cpu')])
-                            mir_path_ = os.path.join(mir_path,'cls_task{}.png'.format(task))
-                            save_image(mir_images, mir_path_, nrow=10)
-                        if 'mir_gen'==args.gen_method:
-                            mir_images = torch.cat([gen_x.to('cpu'),
-                                    gen_mem_x.to('cpu')])
-                            mir_path_ = os.path.join(mir_path,'gen_task{}.png'.format(task))
-                            save_image(mir_images, mir_path_, nrow=10)
-
 
             eval_loaders = [('valid', val_loader), ('test', test_loader)]
 
@@ -433,7 +313,6 @@ for run in range(args.n_runs):
                     #---------------------
                     # Begin Minibatch Eval Loop
                     for i, (data, target) in enumerate(te_loader):
-                        if args.unit_test and i > 10: break
 
                         #if args.cuda:
                         data, target = data.to(args.device), target.to(args.device)
@@ -443,7 +322,7 @@ for run in range(args.n_runs):
                         loss = F.cross_entropy(logits, target)
                         pred = logits.argmax(dim=1, keepdim=True)
 
-                        LOG_temp['acc'] += [pred.eq(target.view_as(pred)).sum().item() / pred.size(0)]
+                        LOG_temp['acc'] += [pred.eq(target.view_as(pred)).sum().item()/pred.size(0)]
                         LOG_temp['cls_loss'] += [loss.item()]
 
                         if gen is not None:
@@ -539,13 +418,6 @@ for mode in ['valid','test']:
     final_elbos = [LOG[x][mode]['final_elbo'] for x in range(args.n_runs)]
     final_elbo_avg = np.mean(final_elbos)
     final_elbo_se = np.std(final_elbos) / np.sqrt(args.n_runs)
-
-    # MIR worked
-    try:
-        final_mir_worked_frac = [LOG[x][mode]['final_mir_worked_frac'] for x in range(args.n_runs)]
-        final_mir_worked_avg = np.mean(final_mir_worked_frac)
-    except:
-        pass
 
     print('\nFinal {} Accuracy: {:.3f} +/- {:.3f}'.format(mode, final_acc_avg, final_acc_se))
     print('\nFinal {} Forget: {:.3f} +/- {:.3f}'.format(mode, final_forget_avg, final_forget_se))
--- pytorch/model.py
+++ pytorch/model.py
@@ -10,103 +10,6 @@ from torch.nn import functional as F
 from VAE.layers import GatedDense
 from utils import Reshape
 
-# Generative Models
-# -----------------------------------------------------------------------------------
-
-# We will use it as an autoencoder for now
-class CVAE(nn.Module):
-    def __init__(self, d, args, **kwargs):
-        super(CVAE, self).__init__()
-
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, d, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),
-
-            nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),
-
-            #nn.Conv2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            #nn.BatchNorm2d(d),
-            #nn.ReLU(inplace=True),
-
-            ResBlock(d, d, bn=True),
-            nn.BatchNorm2d(d),
-            ResBlock(d, d, bn=True),
-        )
-        self.decoder = nn.Sequential(
-            ResBlock(d, d, bn=True),
-            nn.BatchNorm2d(d),
-            ResBlock(d, d, bn=True),
-            nn.BatchNorm2d(d),
-
-            nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),
-
-            #nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            #nn.BatchNorm2d(d),
-            #nn.ReLU(inplace=True),
-
-            nn.ConvTranspose2d(d, d, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(d),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(d, 3, kernel_size=4, stride=2, padding=1, bias=False),
-        )
-        self.f = 4 #8
-        self.d = d
-
-    def encode(self, x):
-        h1 = self.encoder(x)
-        h1 = h1.view(-1, self.d * self.f ** 2)
-        return h1
-        #return self.fc11(h1), self.fc12(h1)
-
-    def reparameterize(self, mu, logvar):
-        if self.training:
-            std = logvar.mul(0.5).exp_()
-            eps = Variable(std.new(std.size()).normal_())
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
-
-    def decode(self, z):
-        z = z.view(-1, self.d, self.f, self.f)
-        h3 = self.decoder(z)
-        return torch.tanh(h3)
-
-    def forward(self, x):
-        #mu, logvar = self.encode(x)
-        #z = self.reparameterize(mu, logvar)
-        #return self.decode(z), mu, logvar
-        hid = self.encode(x)
-        return self.decode(hid), hid
-
-    def sample(self, size):
-        sample = Variable(torch.randn(size, self.d * self.f ** 2), requires_grad=False)
-        if self.cuda():
-            sample = sample.cuda()
-        return self.decode(sample).cpu()
-
-    def loss_function(self, x, recon_x, mu, logvar):
-        self.mse = F.mse_loss(recon_x, x)
-        batch_size = x.size(0)
-
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        self.kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        # Normalise by same number of elements as in reconstruction
-        self.kl_loss /= batch_size * 3 * 1024
-
-        # return mse
-        return self.mse + self.kl_coef * self.kl_loss
 
 class ResBlock(nn.Module):
     def __init__(self, in_channels, channels, bn=False):
@@ -123,11 +26,6 @@ class ResBlock(nn.Module):
 
     def forward(self, x):
         return x + self.convs(x)
-
-
-
-# Classifiers
-# -----------------------------------------------------------------------------------
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
@@ -288,7 +186,7 @@ class MLP(nn.Module):
         out = self.return_hidden(x)
         return self.linear(out)
 
-''' classifier for GEN and GEN-MIR'''
+''' classifier for generative replay'''
 class classifier(nn.Module):
     def __init__(self, args):
         super(classifier, self).__init__()
